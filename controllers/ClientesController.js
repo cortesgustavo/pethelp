1001165434bfe0b9c82d0a338ad6487f5b002b46
Gustavo Miranda Cortes <gustavo.cortes@grupocarrera.local>
2022-03-18 23:41:10 -0300

    Pagina Cliente Configurada para ler dados do JSON - Pets e Consultas

--- controllers/ClientesController.js
+++ controllers/ClientesController.js
@@ -1,7 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+const consultasFilePath = path.join(__dirname, '../data/consultasDataBase.json');
+const petsFilePath = path.join(__dirname, '../data/petsDataBase.json');
+
+const consultas = JSON.parse(fs.readFileSync(consultasFilePath,'utf-8'));
+const pets = JSON.parse(fs.readFileSync(petsFilePath,'utf-8'));
+
 const ClientesController = {
 
     index: (req,res) =>{
-        res.render('clientes');
+        res.render('clientes_json',{consultas, pets});
+        
     },
     cadastroPets:(req,res) =>{
 
